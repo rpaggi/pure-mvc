60c860f62ee7bcef11a84e23ff35266621143ad9
Ronaldo Paggi <ronaldo.paggi@gmail.com>
2018-07-27 14:37:25 -0300

    Complete

--- public/index.php
+++ public/index.php
@@ -1,24 +1,17 @@
 <?php
-require_onde __DIR__.'/../vendor/autoload.php';
-
+require_once __DIR__.'/../vendor/autoload.php';
 use App\Router;
-
 $app = new Router();
-
-$app->get('/', function(){
-   return \App\Controller\AppController::index();
+$app->get('/', function () {
+    return \App\Controller\AppController::index();
 });
-
-$app->get('/list', function(){
-   return \App\Controller\AppController::list();
-})
-
-$app->post('/list', function(){
-   return \App\Controller\AppController::write();
-})
-
+$app->get('/list', function () {
+    return \App\Controller\AppController::list();
+});
+$app->post('/list', function () {
+    return \App\Controller\AppController::write();
+});
 $app->get('/out', function () {
-   return \App\Controller\AppController::logout();
+    return \App\Controller\AppController::logout();
 });
-
 $app->run();
